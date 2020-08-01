039b68c617858561faab66bb8761892a576451e5
TreasureJade <1056024860@qq.com>
2020-08-01 11:07:01 +0800

    树的子结构

--- src/JZ17.java
+++ src/JZ17.java
@@ -1,52 +1,57 @@
-
-
 /**
- * @author Administrator
+ * @author hobo
  * 题目描述：输入两棵二叉树A，B，判断B是不是A的子结构。
  */
 public class JZ17 {
+
+
     public boolean HasSubtree(TreeNode root1, TreeNode root2) {
-        boolean result = false;
-        if (root1!=null&&root2!=null){
-            if (root1.val==root2.val){
-                result = judgeTree(root1,root2);
-            }
-            //如果找不到，那么就再去root的左儿子当作起点，去判断时候包含Tree2
-            if (!result) {
-                result = HasSubtree(root1.left,root2);
-            }
-
-
-            //如果还找不到，那么就再去root的右儿子当作起点，去判断时候包含Tree2
-            if (!result) {
-                result = HasSubtree(root1.right,root2);
-            }
-        }
-        return result;
-    }
-
-    public static boolean judgeTree(TreeNode root1, TreeNode root2) {
-
-        // tree2还没遍历完，1就遍历完成了，返回false
-        if (root2!=null&&root1==null){
+        if (root1 == null || root2 == null) {
             return false;
         }
+        boolean result = false;
+        // 找寻根节点
+        if (root1.val == root2.val) {
+            // 相同从当前位置找
+            result = isChildTree(root1, root2);
+        }
 
-        if (root2==null){
+        // 遍历左子树
+        if (result != true) {
+            result = isChildTree(root1.left, root2);
+        }
+
+        // 遍历右子树
+        if (result != true) {
+            result = isChildTree(root1.right, root2);
+        }
+
+        return result;
+
+    }
+
+    private boolean isChildTree(TreeNode root1, TreeNode root2) {
+        // 遍历完成
+        if (root2 == null) {
             return true;
         }
-
-        if (root1.val!=root2.val){
+        // root2 !=null ，说明树中没有子结构
+        if (root1 == null) {
+            return false;
+        }
+        if (root1.val != root2.val) {
             return false;
         }
 
-        return judgeTree(root1.left,root2.left) &&judgeTree(root1.right,root2.right);
-
+        return isChildTree(root1.left, root2.left) && isChildTree(root1.right, root2.right);
     }
+
+
     class TreeNode {
         public int val;
         public TreeNode right;
         public TreeNode left;
+
         public TreeNode(int x) {
             val = x;
         }
