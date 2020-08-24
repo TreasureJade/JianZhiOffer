519c93e4d8061b156ccc3523d2f594b2d3f134db
TreasureJade <1056024860@qq.com>
2020-08-24 13:59:15 +0800

    动态规划-分割字符串

--- src/dynamic_plan/WordBreak.java
+++ src/dynamic_plan/WordBreak.java
@@ -8,6 +8,9 @@ import java.util.Set;
  * F(i)  前i个字符是否根据字典中的词被成功分词
  * 状态方程：F(i) : 在 j<i中，只要能找到一个F(j)为true，并且从j+2到i之间的字符能在词典中找到，则F(i)为true
  * 初始值：F(0)为true
+ * <a>
+ * https://www.nowcoder.com/practice/5f3b7bf611764c8ba7868f3ed40d6b2c?tpId=46&tqId=29041&tPage=1&rp=1&ru=/ta/leetcode&qru=/ta/leetcode/question-ranking
+ * </a>
  */
 public class WordBreak {
     public boolean wordBreak(String s, Set<String> dict) {
